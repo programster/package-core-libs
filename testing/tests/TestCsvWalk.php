e6fe9424b70ed7e2e476f9eb9ab44ca99290f3cd
Programster <sdpagent@gmail.com>
2021-12-13 16:15:05 +0000

    adds more testing of csv walk

--- testing/tests/TestCsvWalk.php
+++ testing/tests/TestCsvWalk.php
@@ -12,10 +12,19 @@ class TestCsvWalk extends \Programster\CoreLibs\Testing\AbstractTest
 
     public function run()
     {
-        $filepath = __DIR__ . '/../assets/example-csv.csv';
         $this->m_passed = true;
 
-        $callback1 = function(array $row, int $rowIndex) {
+        $this->testNormalCase();
+        $this->testOverridingKeys();
+        $this->testNoHeadersAndOverrideKeys();
+    }
+
+
+    private function testNormalCase() : void
+    {
+        $filepath = __DIR__ . '/../assets/example-csv.csv';
+
+        $callback = function(array $row, int $rowIndex) {
             switch ($rowIndex)
             {
                 case 0:
@@ -78,6 +87,163 @@ class TestCsvWalk extends \Programster\CoreLibs\Testing\AbstractTest
 
         };
 
-        \Programster\CoreLibs\CsvLib::csvWalk($filepath, $callback1);
+        \Programster\CoreLibs\CsvLib::csvWalk($filepath, $callback);
+    }
+
+
+    private function testOverridingKeys() : void
+    {
+        $filepath = __DIR__ . '/../assets/example-csv.csv';
+
+        $callback = function(array $row, int $rowIndex) {
+            switch ($rowIndex)
+            {
+                case 0:
+                {
+                    // should not get here because have headers.
+                    $this->m_passed = false;
+                }
+                break;
+
+                case 1:
+                {
+                    if (count($row) !== 2)
+                    {
+                        $this->m_passed = false;
+                        break;
+                    }
+
+                    if (!array_key_exists('myFirstKey', $row) || !array_key_exists('mySecondKey', $row))
+                    {
+                        $this->m_passed = false;
+                        break;
+                    }
+
+                    if ($row['myFirstKey'] !== 'hello' || $row['mySecondKey'] !== 'world')
+                    {
+                        $this->m_passed = false;
+                        break;
+                    }
+                }
+                break;
+
+                case 2:
+                {
+                    if (count($row) !== 2)
+                    {
+                        $this->m_passed = false;
+                        break;
+                    }
+
+                    if (!array_key_exists('myFirstKey', $row) || !array_key_exists('mySecondKey', $row))
+                    {
+                        $this->m_passed = false;
+                        break;
+                    }
+
+                    if ($row['myFirstKey'] !== 'foo' || $row['mySecondKey'] !== 'bar')
+                    {
+                        $this->m_passed = false;
+                        break;
+                    }
+                }
+                break;
+
+                default:
+                {
+                    // should not get here.
+                    $this->m_passed = false;
+                }
+            }
+
+        };
+
+        \Programster\CoreLibs\CsvLib::csvWalk($filepath, $callback, true, ['myFirstKey', 'mySecondKey']);
+    }
+
+
+    private function testNoHeadersAndOverrideKeys() : void
+    {
+        $filepath = __DIR__ . '/../assets/example-csv.csv';
+
+        $callback = function(array $row, int $rowIndex) {
+            switch ($rowIndex)
+            {
+                case 0:
+                {
+                    // should not get here because have headers.
+                    if (count($row) !== 2)
+                    {
+                        $this->m_passed = false;
+                        break;
+                    }
+
+                    if (!array_key_exists('myFirstKey', $row) || !array_key_exists('mySecondKey', $row))
+                    {
+                        $this->m_passed = false;
+                        break;
+                    }
+
+                    if ($row['myFirstKey'] !== 'first' || $row['mySecondKey'] !== 'second')
+                    {
+                        $this->m_passed = false;
+                        break;
+                    }
+                }
+                break;
+
+                case 1:
+                {
+                    if (count($row) !== 2)
+                    {
+                        $this->m_passed = false;
+                        break;
+                    }
+
+                    if (!array_key_exists('myFirstKey', $row) || !array_key_exists('mySecondKey', $row))
+                    {
+                        $this->m_passed = false;
+                        break;
+                    }
+
+                    if ($row['myFirstKey'] !== 'hello' || $row['mySecondKey'] !== 'world')
+                    {
+                        $this->m_passed = false;
+                        break;
+                    }
+                }
+                break;
+
+                case 2:
+                {
+                    if (count($row) !== 2)
+                    {
+                        $this->m_passed = false;
+                        break;
+                    }
+
+                    if (!array_key_exists('myFirstKey', $row) || !array_key_exists('mySecondKey', $row))
+                    {
+                        $this->m_passed = false;
+                        break;
+                    }
+
+                    if ($row['myFirstKey'] !== 'foo' || $row['mySecondKey'] !== 'bar')
+                    {
+                        $this->m_passed = false;
+                        break;
+                    }
+                }
+                break;
+
+                default:
+                {
+                    // should not get here.
+                    $this->m_passed = false;
+                }
+            }
+        };
+
+        \Programster\CoreLibs\CsvLib::csvWalk($filepath, $callback, true, ['myFirstKey', 'mySecondKey']);
     }
 }
